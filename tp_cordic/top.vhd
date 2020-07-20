cc4545377da3840a204d666db640c0d367f9cdef
Nacho <carballeda.ignacio@gmail.com>
2020-07-20 19:00:27 -0300

    style sig

--- tp_cordic/top.vhd
+++ tp_cordic/top.vhd
@@ -93,16 +93,16 @@ architecture top_level_arq of top_level is
 
 -- Variables auxiliares para pasar datos a las funciones --
 
-	signal aux_pixel_x_i: std_logic_vector(9 downto 0) := "0000000000";
-	signal aux_pixel_y_i: std_logic_vector(9 downto 0) := "0000000000";
+	signal sig_aux_pixel_x_i: std_logic_vector(9 downto 0) := "0000000000";
+	signal sig_aux_pixel_y_i: std_logic_vector(9 downto 0) := "0000000000";
 
     -- Utilizada para entender si estamos leyendo un valor de X, Y o Z (de RAM)
     signal xyz_selector_current, xyz_selector_next: natural := 0;
 	
 	-- VGA
-	signal blue_enable: std_logic;
-	signal red_enable: std_logic;
-	signal green_enable: std_logic;
+	signal sig_blue_enable: std_logic;
+	signal sig_red_enable: std_logic;
+	signal sig_green_enable: std_logic;
 	
 	-- Coordenadas
     signal x_coord_current, x_coord_next: std_logic_vector(COORDS_WIDTH-1 downto 0) := (others => '0');
@@ -110,30 +110,29 @@ architecture top_level_arq of top_level is
     signal z_coord_current, z_coord_next: std_logic_vector(COORDS_WIDTH-1 downto 0) := (others => '0');
 	
 	-- LEDS
-	signal led_aux: std_logic_vector(3 downto 0) := "0000";
+	signal sig_led_aux: std_logic_vector(3 downto 0) := "0000";
 	
 	-- UART
 	constant Divisor : std_logic_vector := "000000011011"; -- Divisor=27 para 115200 baudios
-	signal sig_Din	: std_logic_vector(7 downto 0);
-	signal sig_Dout	: std_logic_vector(7 downto 0);
-	signal sig_RxErr	: std_logic;
-	signal sig_RxRdy	: std_logic;
-	signal sig_TxBusy	: std_logic;
-	signal sig_StartTx: std_logic;
-	signal r_data: std_logic_vector(7 downto 0);
+	signal sig_uart_data_in	: std_logic_vector(7 downto 0);
+	signal sig_uart_data_out	: std_logic_vector(7 downto 0);
+	signal sig_uart_rx_error	: std_logic;
+	signal sig_uart_rx_ready	: std_logic;
+	signal sig_uart_tx_busy	: std_logic;
+	signal sig_uart_tx_start: std_logic;
+	signal sig_uart_readed_data: std_logic_vector(7 downto 0);
 
     -- RAM
-    signal data_in: std_logic_vector(DATA_WIDTH-1 downto 0) := (others => '0');
-    signal address_in: std_logic_vector(ADDRESS_WIDTH-1 downto 0) := (others => '0');
-    signal data_out: std_logic_vector(DATA_WIDTH-1 downto 0);
+    signal sig_ram_address_in: std_logic_vector(ADDRESS_WIDTH-1 downto 0) := (others => '0');
+    signal sig_ram_data_out: std_logic_vector(DATA_WIDTH-1 downto 0);
 
 	-- Dual Port RAM (for video)
-	signal vram_rst : std_logic;
-	signal vram_data_wr : std_logic_vector(DPRAM_BYTES_WIDTH*8-1 downto 0);
-	signal vram_addr_wr : std_logic_vector(DPRAM_ADDR_BITS-1 downto 0);
-	signal vram_ena_wr : std_logic;
-	signal vram_addr_rd : std_logic_vector(DPRAM_ADDR_BITS-1 downto 0);
-	signal vram_data_rd : std_logic_vector(DPRAM_BYTES_WIDTH*8-1 downto 0);
+	signal sig_vram_rst : std_logic;
+	signal sig_vram_data_wr : std_logic_vector(DPRAM_BYTES_WIDTH*8-1 downto 0);
+	signal sig_vram_addr_wr : std_logic_vector(DPRAM_ADDR_BITS-1 downto 0);
+	signal sig_vram_ena_wr : std_logic;
+	signal sig_vram_addr_rd : std_logic_vector(DPRAM_ADDR_BITS-1 downto 0);
+	signal sig_vram_data_rd : std_logic_vector(DPRAM_BYTES_WIDTH*8-1 downto 0);
 		
     -- State Machine
     type state_t is (initial_state, waiting_for_uart, waiting_for_sram,
@@ -165,7 +164,7 @@ begin
     end process;
 
   -- next state logic
-    process(r_data, address_current, state_current, rw_current, xyz_selector_current,
+    process(sig_uart_readed_data, address_current, state_current, rw_current, xyz_selector_current,
     data_in_current, address_current, cycles_current, rst_tl, bytes_received_current)
     begin
         -- default values
@@ -175,7 +174,7 @@ begin
         bytes_received_next <= bytes_received_current;
         case state_current is
             when initial_state =>
-				led_aux <= "0000";
+				sig_led_aux <= "0000";
                 if cycles_current = 0 then
                     state_next <= waiting_for_uart;
                 else
@@ -183,8 +182,8 @@ begin
                     state_next <= initial_state;
                 end if;
             when waiting_for_uart =>
-				led_aux <= "0001";
-                if sig_RxRdy = '1' then
+				sig_led_aux <= "0001";
+                if sig_uart_rx_ready = '1' then
                     state_next <= reading_from_uart;
                 elsif bytes_received_current = BYTES_TO_RECEIVE then
                     state_next <= uart_end_data_reception;
@@ -192,7 +191,7 @@ begin
                     state_next <= waiting_for_uart;
                 end if;
             when reading_from_uart =>
-				data_in_next <= r_data;
+				data_in_next <= sig_uart_readed_data;
 				state_next <= write_sram;
 				bytes_received_next <= bytes_received_current + 1;
             when write_sram =>
@@ -207,19 +206,19 @@ begin
 				rw_next <= "0"; -- Vamos a necesitar leer
                 address_next <= 0; -- La prox address de RAM que nos interesa es 0
 			when idle =>
-				led_aux <= "0010";
+				sig_led_aux <= "0010";
 				state_next <= print;
 			when print =>
-				led_aux <= "0011";
+				sig_led_aux <= "0011";
 				case xyz_selector_current is
 					when 0 =>
-						x_coord_next <= data_out;
+						x_coord_next <= sig_ram_data_out;
 						xyz_selector_next <= xyz_selector_current + 1;
 					when 1 =>
-						y_coord_next <= data_out;
+						y_coord_next <= sig_ram_data_out;
 						xyz_selector_next <= xyz_selector_current + 1;
 					when 3 =>
-						z_coord_next <= data_out;
+						z_coord_next <= sig_ram_data_out;
 						xyz_selector_next <= xyz_selector_current + 1;
 					when others =>
 						xyz_selector_next <= 0;
@@ -240,28 +239,28 @@ begin
 			red_o => red_out_tl,
 			grn_o => grn_out_tl,
 			blu_o => blu_out_tl,
-			red_i => red_enable,
-			grn_i => green_enable,
-			blu_i => blue_enable,
-			pixel_row => aux_pixel_y_i,
-			pixel_col => aux_pixel_x_i
+			red_i => sig_red_enable,
+			grn_i => sig_green_enable,
+			blu_i => sig_blue_enable,
+			pixel_row => sig_aux_pixel_y_i,
+			pixel_col => sig_aux_pixel_x_i
 			);
 	
 	
 	-- LEDS
-	led_tl <= not led_aux;
+	led_tl <= not sig_led_aux;
 	
 	-- RAM
 	ram_internal : RAM
 	port map (
 		clka => clk_tl,
 		wea => rw_current,
-		addra => address_in,
+		addra => sig_ram_address_in,
 		dina => data_in_current,
-		douta => data_out
+		douta => sig_ram_data_out
 	);
 	
-	address_in <= std_logic_vector(to_unsigned(address_current, ADDRESS_WIDTH));
+	sig_ram_address_in <= std_logic_vector(to_unsigned(address_current, ADDRESS_WIDTH));
 	
 	-- UART Instanciation :
 	uart_load_data : uart
@@ -273,12 +272,12 @@ begin
 	port map (
 		Rx	=> rx_tl,
 	 	Tx	=> tx_tl,
-	 	Din => sig_Din,
-	 	StartTx => sig_StartTx,
-		TxBusy => sig_TxBusy,
-		Dout	=> sig_Dout,
-		RxRdy	=> sig_RxRdy,
-		RxErr	=> sig_RxErr,
+	 	Din => sig_uart_data_in,
+	 	StartTx => sig_uart_tx_start,
+		TxBusy => sig_uart_tx_busy,
+		Dout	=> sig_uart_data_out,
+		RxRdy	=> sig_uart_rx_ready,
+		RxErr	=> sig_uart_rx_error,
 		Divisor	=> Divisor,
 		clk	=> clk_tl,
 		rst	=> rst_tl
@@ -290,13 +289,13 @@ begin
 		ADDR_BITS => DPRAM_ADDR_BITS
 	)
 	port map(
-		rst => vram_rst,
+		rst => sig_vram_rst,
 		clk => clk_tl,
-		data_wr => vram_data_wr,
-		addr_wr => vram_addr_wr,
-		ena_wr  => vram_ena_wr,
-		data_rd => vram_data_rd,
-		addr_rd => vram_addr_rd
+		data_wr => sig_vram_data_wr,
+		addr_wr => sig_vram_addr_wr,
+		ena_wr  => sig_vram_ena_wr,
+		data_rd => sig_vram_data_rd,
+		addr_rd => sig_vram_addr_rd
 	);
 	
 end top_level_arq;
