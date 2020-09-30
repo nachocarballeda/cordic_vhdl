2bb40fefcb2826e868d577eb17415732ccf2229d
Nacho <carballeda.ignacio@gmail.com>
2020-09-29 22:24:39 -0300

    working on clean folder..

--- utiles/send_coords.py
+++ utiles/send_coords.py
@@ -2,18 +2,18 @@
 
 import serial
 
-COORDS_WIDTH = 7
+COORDS_WIDTH = 8
 LINES_TOTAL = 2961 #11946 Saque unas lineas para necesitar un bit menos de RAM addr
 
-with open('coords.txt') as fp, serial.Serial('/dev/ttyUSB0', 115200, timeout=1) as serial:
+with open('coordenadas.txt') as fp, serial.Serial('/dev/ttyUSB0', 115200, timeout=1) as serial:
     for line_number, line in enumerate(fp):
         if line_number < LINES_TOTAL:
 
-            x = int(float(line.split('\t')[0])*2**COORDS_WIDTH)
+            x = int(float(line.split('\t')[0])*2**(COORDS_WIDTH-1))
             x_signed = True if x < 0 else False
-            y = int(float(line.split('\t')[1])*2**COORDS_WIDTH)
+            y = int(float(line.split('\t')[1])*2**(COORDS_WIDTH-1))
             y_signed = True if y < 0 else False
-            z = -1*int(float(line.split('\t')[2])*2**COORDS_WIDTH)
+            z = -1*int(float(line.split('\t')[2])*2**(COORDS_WIDTH-1))
             z_signed = True if z < 0 else False
 
             serial.write((x).to_bytes(1, 'big', signed=x_signed))
